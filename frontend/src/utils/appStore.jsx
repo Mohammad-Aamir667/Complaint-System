a535479e25e9d52e10c18f1cf28ab8d320dd50bd
Mohammad Aamir <aamireverlasting786>
2025-08-12 01:06:50 +0530

    added superadmin reducer to store

--- frontend/src/utils/appStore.jsx
+++ frontend/src/utils/appStore.jsx
@@ -3,14 +3,21 @@ import userReducer from "./userSlice";
 import userComplaintReducer from "./userComplaintsSlice";
 import adminComplaintReducer from "./adminComplaintSlice";
 import managerDataReducer from "./managerDataSlice";
+import adminDataReducer from "./adminDataSlice";
 import notificationReducer from "./notificationSlice"
+import managerComplaintReducer from "./managerComplaintSlice";
+import superAdminComplaintReducer from "./superAdminComplaintsSlice";
 const appStore = configureStore({
   reducer:{
         user:userReducer,
         userComplaints:userComplaintReducer,
         adminComplaints:adminComplaintReducer,
         managerData:managerDataReducer,
-        notifications:notificationReducer
+        notifications:notificationReducer,
+        managerComplaints:managerComplaintReducer,
+        adminData:adminDataReducer,
+        superAdminComplaints:superAdminComplaintReducer
+
   },
 });
 export default appStore; 
