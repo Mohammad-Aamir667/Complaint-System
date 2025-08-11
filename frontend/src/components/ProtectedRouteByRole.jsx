10ddc902aa6e2848fba720aca9397e887f106a9a
Mohammad Aamir <aamireverlasting786>
2025-08-12 03:38:46 +0530

    route modified

--- frontend/src/components/ProtectedRouteByRole.jsx
+++ frontend/src/components/ProtectedRouteByRole.jsx
@@ -7,10 +7,8 @@ const ProtectedRouteByRole = ({ allowedRoles, children }) => {
   const user = useSelector((store) => store.user);
   const location = useLocation();
   const navigate = useNavigate();
-
   useEffect(() => {
     if (user && !allowedRoles.includes(user.role)) {
-      // Redirect based on current user role
       if (user.role === "employee") {
         navigate("/employee/dashboard");
       } else if (user.role === "manager") {
@@ -21,17 +19,14 @@ const ProtectedRouteByRole = ({ allowedRoles, children }) => {
         navigate("/superadmin/dashboard");
       }
       else {
-        navigate(-1); // fallback to previous page
+        navigate(-1); 
       }
     }
   }, [user]);
 
-  // Show loading while user is not yet loaded
   if (!user) return <div className="justify-center items-center flex flex-col h-screen">
     <Loader2 className=" h-8 w-8 animate-spin text-blue-500" />
   </div>;
-
-  // If role is allowed, render the children (protected route)
   return allowedRoles.includes(user.role) ? children : null;
 };
 
